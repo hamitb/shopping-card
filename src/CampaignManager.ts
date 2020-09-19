421040ac63fd1b24a3587c3e4407bffbf27a89cf
Hamit Burak Emre <hamitburakemre@gmail.com>
2020-09-20 01:33:26 +0300

    Add finish campaign method

--- src/CampaignManager.ts
+++ src/CampaignManager.ts
@@ -5,11 +5,19 @@ import { Nullable } from "./Types";
 export default class CampaignManager {
     private static campaigns: Map<Category, Campaign> = new Map();
 
-    public static activate(campaign: Campaign) {
+    public static start(campaign: Campaign) {
         CampaignManager.campaigns.set(campaign.category, campaign);
         campaign.active = true;
+    }
 
-        return campaign;
+    public static finish(campaign: Campaign) {
+        const category = campaign.category;
+        const gotCampaign = CampaignManager.campaigns.get(category);
+
+        if (gotCampaign == campaign) {
+            CampaignManager.campaigns.delete(category);
+            campaign.active = false;
+        }
     }
 
     public static getFor(category: Nullable<Category>): Nullable<Campaign> {
--- test/CampaignManager.test.ts
+++ test/CampaignManager.test.ts
@@ -3,15 +3,48 @@ import Category from '../src/Category';
 import Campaign from '../src/Campaign';
 
 describe('campaign manager', () => {
-    test('should be able to activate and get campaigns', () => {
+    test('should be able to start and get campaigns', () => {
         const category = new Category('Food');
         const campaign = new Campaign(category, 10.0);
 
-        CampaignManager.activate(campaign);
+        CampaignManager.start(campaign);
 
         const gotCampaign = CampaignManager.getFor(category);
 
         expect(gotCampaign).toBe(campaign);
+    });
+
+    test('should be able to finish a campaign', () => {
+        const category = new Category('Food');
+        const campaign = new Campaign(category, 10.0);
+
+        CampaignManager.start(campaign);
+
+        let gotCampaign: any = CampaignManager.getFor(category);
+
+        expect(gotCampaign).toBe(campaign);
+        expect(gotCampaign.active).toBeTruthy();
+
+        CampaignManager.finish(campaign);
+
+        gotCampaign = CampaignManager.getFor(category);
+
+        expect(gotCampaign).toBeUndefined();
+        expect(campaign.active).toBeFalsy();
+    });
+
+    test('should do nothing if campaign not exist while finishing', () => {
+        const category = new Category('Food');
+        const campaign1 = new Campaign(category, 10.0);
+        const campaign2 = new Campaign(category, 20.0);
+
+        CampaignManager.start(campaign1);
+        CampaignManager.finish(campaign2);
+
+        const gotCampaign: any = CampaignManager.getFor(category);
+
+        expect(gotCampaign).toBe(campaign1);
+        expect(gotCampaign.active).toBeTruthy();
     });
 
     test('should return parent category campaign', () => {
@@ -19,7 +52,7 @@ describe('campaign manager', () => {
         const childCategory = new Category('Shirts', parentCategory);
         const campaign = new Campaign(parentCategory, 10.0);
 
-        CampaignManager.activate(campaign);
+        CampaignManager.start(campaign);
 
         expect(CampaignManager.getFor(childCategory)).toBe(campaign);
     })
